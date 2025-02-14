0bd019ae48b40886427ada1e8a2e5ff342226344
souzap371 <souzap371@gmail.com>
2025-02-14 08:32:01 -0500

    commit inicial

---
+++ src/test/java/com/example/crm/integration/IntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crm.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
